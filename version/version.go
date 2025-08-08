abe9a4f7fbe724017cf08dbf402f9c77c1505f34
Dennis Carey <dennis.carey@users.noreply.github.com>
2025-08-08 09:34:16 -0400

    Update the version

--- version/version.go
+++ version/version.go
@@ -6,7 +6,7 @@ package version
 import "github.com/hashicorp/packer-plugin-sdk/version"
 
 var (
-	Version           = "1.3.10"
+	Version           = "1.3.11"
 	VersionPrerelease = "dev"
 	VersionMetadata   = ""
 	PluginVersion     = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
